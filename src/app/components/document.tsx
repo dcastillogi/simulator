587be6b1332d2ee7e3b3606fc4c90e658f190d40
Daniel Castillo <dc.giraldocg@hotmail.com>
2024-03-22 04:12:33 -0500

    editor js fixing errors

--- src/app/components/document.tsx
+++ src/app/components/document.tsx
@@ -1,26 +1,37 @@
-import EditorJS from '@editorjs/editorjs';
+import EditorJS from "@editorjs/editorjs";
 // @ts-ignore
-import Table from '@editorjs/table'
+import Table from "@editorjs/table";
 import "./editorjs.css";
-import { useEffect } from 'react';
+import { useEffect } from "react";
 
-export default function Document({width}: {width: number}) {
+export default function Document({ width }: { width: number }) {
     useEffect(() => {
         const editor = new EditorJS({
-            placeholder: 'Type your notes or document here',
+            placeholder: "Type your notes or document here",
             tools: {
                 table: Table,
-              }        
-        })
+            },
+        });
+
         return () => {
-            editor.destroy();
-        }
-    }, [])
+            editor.isReady.then(() => {
+                editor.destroy();
+            });
+        };
+    }, []);
     return (
-        <div className="w-full h-full bg-background-dimmed py-8 overflow-y-auto" style={{width: `${width}px`}}>
+        <div
+            className="w-full h-full bg-background-dimmed py-8 overflow-y-auto"
+            style={{ width: `${width}px` }}
+        >
             <div className="max-w-xl mx-auto px-6">
-                <h1 className="text-placeholder font-semibold text-3xl mb-7" contentEditable>Untitled Simulation</h1>
-                <div id="editorjs" className='text-primary font-light'></div>
+                <h1
+                    className="text-placeholder font-semibold text-3xl mb-7 outline-none"
+                    contentEditable
+                >
+                    Untitled Simulation
+                </h1>
+                <div id="editorjs" className="text-primary font-light"></div>
             </div>
         </div>
     );
